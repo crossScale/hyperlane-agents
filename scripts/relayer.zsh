5b60e7aa8d2069ac806e9e61144fa5faee84fef9
Patipat Luangwirun <2pats@patipats-mbp.lan>
2023-03-25 20:48:11 -0400

    validators & relayers for hyperlane

---
+++ scripts/relayer.zsh
@@ -0,0 +1,14 @@
+while [[ "$#" -gt 0 ]]
+do case $1 in
+    -C|--chain) chain="$2"
+    shift;;
+    *) echo "Unknown parameter passed: $1"
+    exit 1;;
+esac
+shift
+done
+echo "Running Relayer for $chain"
+
+env_file="relayer.$chain.env"
+
+docker run -it --mount type=bind,source=/Users/2pats/Desktop/code/xscale/hyperlane-agents/config/custom.json,target=/config/custom.json,readonly -e CONFIG_FILES=/config/custom.json --env-file $env_file gcr.io/abacus-labs-dev/hyperlane-agent:1cbe5fd-20230309-202035 ./relayer
